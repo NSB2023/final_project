297959592ca2d979a2cf47eb0e5bd1eab938b0c4
NSB2023 <sakibscl@gmail.com>
2026-02-13 12:18:56 -0500

    Prepared project for deployment

--- server.py
+++ server.py
@@ -41,5 +41,10 @@ def emotion_detector_route():
     )
 
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
